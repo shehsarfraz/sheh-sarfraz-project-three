e771d61e38fdc3ea4933a8f51afd260ece1fd0c7
shehsarfraz <anushehsarfraz@gmail.com>
2023-05-16 10:57:24 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then( (response) => {
+    return response.json()
+  })
+  .then( (jsonResult) => {
+    console.log('It worked', jsonResult)
+  })
+
+
+  // fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
+  // .then(function(response){
+  //   return response.json();
+  // })
+  // .then(function(jsonResult){
+  //   console.log('It worked!', jsonResult);
+  // })
+
+
+// Set up fetch request 
+// Pull random recipe's name, ingredients, instructions and picture 
+// Display above within it's own component and render that component within app
+// Create function using terniaries to allow user to clear current drink or show recipe 
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
